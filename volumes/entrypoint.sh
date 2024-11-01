8a8a6e04a3d78854b81d04c1609ae859d0b5015f
Julian A Rendon <91917978+jrendon102@users.noreply.github.com>
2024-11-01 12:01:07 -0400

    MD-03: Connect Camera(s) To Docker Containers (#4)
    ### Problem
    - Need to mount video devices on to the docker container.
    - When mounting the video devices, camera permission issues occurs when trying to run camera server node in container.
    
    ### Solution
    - Add /dev/video0 as a volume in docker-compose-template.yml.
    - Modify entrypoint.sh to update permission and ownership of /dev/video* to container.
    
    ### Notes
    - So far only the camera server was tested. Still need to test whether camera client works.
    - Might have to find a better way to mount all video devices. Docker compose doesn't support wildcards

--- volumes/entrypoint.sh
+++ volumes/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+USERNAME=$(whoami)
+
 echo "Running Entrypoint"
+
+# Source the ROS setup script
 source /opt/ros/iron/setup.bash
 echo "source /opt/ros/iron/setup.bash" >> ~/.bashrc
+
+# Change ownership of video devices to current user and
+# set appropriate permissons
+sudo chown $USERNAME:video /dev/video*
+sudo chmod 660 /dev/video*
 
 # Keep the container running
 exec tail -f /dev/null
