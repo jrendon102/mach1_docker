2199356b1765b40036892e04a27eaec9b69f47da
Julian A Rendon <julianrendon514@gmail.com>
2024-11-06 23:17:37 -0500

    MD-04: Update entrypoint.sh

--- volumes/entrypoint.sh
+++ volumes/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-USERNAME=$(whoami)
-
 echo "Running Entrypoint"
 
-# Source the ROS setup script
+# Source the ROS setup script abd append the source
+# command to .bashrc for future terminal sessions
 source /opt/ros/iron/setup.bash
 echo "source /opt/ros/iron/setup.bash" >> ~/.bashrc
 
-# Change ownership of video devices to current user and
-# set appropriate permissons
+# Change ownership of video devices to the current user 
+# and set appropriate permissions
+USERNAME=$(whoami)
 sudo chown $USERNAME:video /dev/video*
 sudo chmod 660 /dev/video*
 
-# Keep the container running
+# Create a .Xauthority file if it doesn't exist
+touch /home/$USERNAME/.Xauthority
+
+# Add the X11 authentication cookie to the .Xauthority file
+xauth add "$DISPLAY" "$XAUTH_PROTOCOL" "$XAUTH_KEY"
+
+# Keep the container running indefinitely
 exec tail -f /dev/null
