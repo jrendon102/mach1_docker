2efdef80327d5fade9bd376be7161d0aa6709e21
Julian A Rendon <91917978+jrendon102@users.noreply.github.com>
2024-11-08 14:32:00 -0500

    MD-04: Enhance Docker Configuration (#5)
    Problem
    - Need to enhance the docker setup to build and start containers for better deployment of robot.
    
    Solution
    - Reduced size of repo by eliminating unnecessary files.
    - Updated entrypoint.sh to build and start robotics related source code.
    - Created launch_robot.sh script to build docker image and run container.
    
    Notes
    - N/A

--- volumes/entrypoint.sh
+++ volumes/entrypoint.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-USERNAME=$(whoami)
-
 echo "Running Entrypoint"
 
-# Source the ROS setup script
+# Source the ROS setup script abd append the source
+# command to .bashrc for future terminal sessions
 source /opt/ros/iron/setup.bash
 echo "source /opt/ros/iron/setup.bash" >> ~/.bashrc
 
-# Change ownership of video devices to current user and
-# set appropriate permissons
+# Change ownership of video devices to the current user 
+# and set appropriate permissions
+USERNAME=$(whoami)
 sudo chown $USERNAME:video /dev/video*
 sudo chmod 660 /dev/video*
 
-# Keep the container running
+# Create a .Xauthority file if it doesn't exist
+touch /home/$USERNAME/.Xauthority
+
+# Add the X11 authentication cookie to the .Xauthority file
+xauth add "$DISPLAY" "$XAUTH_PROTOCOL" "$XAUTH_KEY"
+
+MACHX_CODE_DIR="/home/$USERNAME/machx_code"
+if [ -d "$MACHX_CODE_DIR" ]; then
+    echo "Navigating to ROS workspace at $MACHX_CODE_DIR"
+    cd "$MACHX_CODE_DIR"
+
+    echo "Building ROS workspace"
+    colcon build
+
+    source "$MACHX_CODE_DIR/install/setup.bash"
+    echo "source $MACHX_CODE_DIR/install/setup.bash" >> ~/.bashrc
+else
+    echo "ROS workspace directory not found at $MACHX_CODE_DIR"
+fi
+
+# Keep the container running indefinitely
 exec tail -f /dev/null
